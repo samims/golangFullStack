4902377417dcc42c99576d437404453fb7cc7e59
Samiul Sk <sam91v@gmail.com>
2019-11-24 16:20:44 +0530

    response and format error added

---
+++ api/response/json.go
@@ -0,0 +1,28 @@
+package response
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		_, _ = fmt.Fprint(w, err.Error())
+	}
+}
+
+func Error(w http.ResponseWriter, statusCode int, err error) {
+	if err != nil {
+		JSON(w, statusCode, struct {
+			Error string `json:"error"`
+		}{
+			Error: err.Error(),
+		})
+		return
+	}
+	JSON(w, http.StatusBadRequest, nil)
+
+}
